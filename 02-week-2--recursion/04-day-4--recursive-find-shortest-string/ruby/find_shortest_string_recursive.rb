8e0a5558b91d5addb308c53a9ffac0da2cf900b6
Adam DePaolo <amdepaolo@gmail.com>
2023-07-26 11:44:27 -0400

    adds solution for shortest string ruby

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
@@ -1,5 +1,13 @@
 def find_shortest_string_recursive(arr)
   # type your code in here
+  if arr.length == 1
+    return arr.first
+  elsif arr[0].length <= arr[1].length
+    arr.delete_at 1
+    find_shortest_string_recursive(arr)
+  else find_shortest_string_recursive(arr[1..-1])
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -20,4 +28,7 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+# if array has length of 1 return first item
+# else compare
 # And a written explanation of your solution
+# If the array has only one element that means it is the shortest by default, so we can compare the elements, removing it from the array until their is only one left 
