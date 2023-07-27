467131e840a63937cb71903865de3e3f7799a76e
Adam DePaolo <amdepaolo@gmail.com>
2023-07-27 16:26:34 -0400

    added recursive selection sort js  solution

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,6 +1,31 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  const sorted = []
+  if (arr.length < 1){
+    return [...sorted]
+  }
+  else {
+    const min = Math.min(...arr);
+    const idx = arr.indexOf(min);
+    arr.splice(idx,1)
+    return[...sorted, min, ...selectionSortRecursive(arr)]
+  }
+
 }
+
+// function selectionSort(arr) {
+//   const sorted = [];
+
+//   while (arr.length > 0) {
+//     const min = Math.min(...arr);
+//     const idx = arr.indexOf(min);
+
+//     sorted.push(min);
+//     arr.splice(idx, 1);
+//   }
+
+//   return sorted;
+// }
 
 if (require.main === module) {
   // add your own tests in here
@@ -14,3 +39,4 @@ module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// The problem with solving this problem recursively if we declare a sorted array variable, each time the recursive algorithm runs it will be
